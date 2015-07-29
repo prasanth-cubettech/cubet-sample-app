4d2f0261cd90512347bd0605198a0bc939ea5911
prasanth <prasanth@cubettech.com>
2015-07-29 19:22:06 +0530

    Set up Puma, Nginx & Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :branch, "master"
 set :user,            'ubuntu'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
-
+set :rbenv_ruby, '2.2.1'
 # Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
