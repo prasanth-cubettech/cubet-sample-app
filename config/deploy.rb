2901a7a6d17974d6ddb6b365db4efe77a8034217
prasanth <prasanth@cubettech.com>
2015-07-30 09:44:44 +0530

    Updated nginx.conf and deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
-server '54.68.97.110', port: 80, roles: [:web, :app, :db], primary: true
+server '54.68.97.110', port: 22, roles: [:web, :app, :db], primary: true
 
 set :application, 'cubet-sample-app'
 set :repo_url, 'git@github.com:prasanth-cubettech/cubet-sample-app.git'
@@ -12,7 +12,7 @@ set :branch, "master"
 set :user,            'ubuntu'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
-set :rbenv_ruby, '2.2.1'
+
 # Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
@@ -24,7 +24,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, auth_methods: ["publickey"], keys: ["/home/dev001/key/ffpeg-key.pem"] }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
